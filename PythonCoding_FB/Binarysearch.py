6e4bedfb8b5ff5994a10c021baa16def38e0df35
Abhay Kela <abhaysk2004@gmail.com>
2024-05-14 19:35:42 -0400

    added notes for algorithm constraints

--- PythonCoding_FB/Binarysearch.py
+++ PythonCoding_FB/Binarysearch.py
@@ -1,3 +1,4 @@
+#This algorithm only works when array is in ascending order
 def binary_search(arr, target):
     low, high = 0, len(arr) - 1
     while low <= high:
@@ -10,8 +11,8 @@ def binary_search(arr, target):
             high = mid - 1
     return -1 #returns -1 if target is not found
 
-arr = [2,3,5,7,8]
-target = 5
+arr = [2,3,7,8,5,8,9,10]
+target = 8
 result = binary_search(arr, target)
 if result != -1:
     print(f"Element {target} found at index {result}")
